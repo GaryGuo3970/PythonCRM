be621538fcedbacdb80a312073f1d70b36ffbf9b
garyguo <gary.guo@incadea.com>
2025-04-16 09:54:36 +0800

    middleware

--- pythoncrm/crm/middleware/loginmiddleware.py
+++ pythoncrm/crm/middleware/loginmiddleware.py
@@ -22,6 +22,18 @@ class LoginMiddleWare(MiddlewareMixin):
             return
         
         return redirect("/login")
+    
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        print("测试中间件：视图", view_func, view_args, view_kwargs)
+        # 1 .获取当前请求的url
+        # 2 .获取当前请求的视图函数
+        # 3 .获取当前用户的权限
+        # 4 .获取当前用户的角色
+        # 5 .检查当前用户的权限是否有访问该视图函数的权限
+        # 6 .如果没有权限，返回403错误
+        # 7 .如果有权限，返回None，继续执行视图函数
+        # 8 .如果没有登录，返回登录页面
+        return None
 
     def process_response(self, request, response):
         print("测试中间件：相应", response)
